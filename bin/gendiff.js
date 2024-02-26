8548afdd0bb7d01320fc7a5f2b47ab67bb1ec821
Ilya Roschikhmarov <ilrosch@mail.ru>
2024-02-26 13:51:55 +0300

    modification of the utility reference information

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,7 +5,9 @@ const program = new Command();
 
 program
   .name('gendiff')
+  .arguments('<filepath1> <filepath2>')
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1', '-V, --version', 'output the version number')
   .helpOption('-h, --help', 'output usage information')
+  .option('-f, --format [type]', 'output format')
   .parse();
